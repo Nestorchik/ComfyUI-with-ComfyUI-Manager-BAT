242a9ed5d2f0232d495787ff238635c4b023cec4
Andy Resha <andyr@mail.ru>
2024-04-21 20:26:41 +0300

    Update install_comfyui.bat

--- install_comfyui.bat
+++ install_comfyui.bat
@@ -1,5 +1,5 @@
 echo off
-REM This file installs the git-version of "ComfyUI" + "ComfyUI-Manager" with "torch-cuda" + "xFormers" for NVIDIA videocards.
+REM This file installs the git-version of "ComfyUI" + "ComfyUI-Manager" with "torch-cuda" for NVIDIA videocards.
 cls
 echo Make sure that GIT is in the PATH
 echo Change "python=c:\p31\python.exe" to the actual location of python 3.10 in the system.
@@ -26,11 +26,11 @@ echo Uninstalling CPU python packages
 %scripts%\python -m pip uninstall torch -y
 echo ======================================
 echo Installing GPU-cuda TORCH python packages
-%scripts%\python -m pip install torch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 --index-url https://download.pytorch.org/whl/cu121
+%scripts%\python -m pip install torch torchvision --index-url https://download.pytorch.org/whl/cu121
 echo ======================================
-echo Installing GPU-cuda xFormers python packages
-%scripts%\python -m pip install -U xformers==0.0.23.post1 --index-url https://download.pytorch.org/whl/cu121
-echo ======================================
+REM echo Installing GPU-cuda xFormers python packages
+REM %scripts%\python -m pip install -U xformers==0.0.23.post1 --index-url https://download.pytorch.org/whl/cu121
+REM echo ======================================
 echo Installing ComfyUI-Manager custom_node
 cd %comfy%\custom_nodes
 git clone https://github.com/ltdrdata/ComfyUI-Manager.git
